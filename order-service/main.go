4dbaefdd55666ca4ed36df8469c2b0fa981af1ed
SanjaiPG <sanjaigukesan8686@gmail.com>
2026-02-20 12:20:57 +0530

    transaction logic implemented

--- order-service/main.go
+++ order-service/main.go
@@ -44,6 +44,7 @@ func main() {
 	fmt.Println("Connected to PostgreSQL successfully")
 
 	http.HandleFunc("/health", healthCheck)
+	http.HandleFunc("/place-order", placeOrder)
 
 	fmt.Println("Order Service running on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
@@ -51,4 +52,74 @@ func main() {
 
 func healthCheck(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Order Service is running"))
+}
+
+func placeOrder(w http.ResponseWriter, r *http.Request) {
+
+	userID := 1
+	productID := 1
+	price := 1000
+
+	tx, err := db.Begin()
+	if err != nil {
+		http.Error(w, "Failed to start transaction", 500)
+		return
+	}
+
+	var stock int
+	err = tx.QueryRow("SELECT stock FROM inventory WHERE product_id=$1 FOR UPDATE", productID).Scan(&stock)
+	if err != nil {
+		tx.Rollback()
+		http.Error(w, "Product not found", 400)
+		return
+	}
+
+	if stock <= 0 {
+		tx.Rollback()
+		http.Error(w, "Out of stock", 400)
+		return
+	}
+
+	var balance int
+	err = tx.QueryRow("SELECT balance FROM users WHERE user_id=$1 FOR UPDATE", userID).Scan(&balance)
+	if err != nil {
+		tx.Rollback()
+		http.Error(w, "User not found", 400)
+		return
+	}
+
+	if balance < price {
+		tx.Rollback()
+		http.Error(w, "Insufficient balance", 400)
+		return
+	}
+
+	_, err = tx.Exec("UPDATE inventory SET stock=stock-1 WHERE product_id=$1", productID)
+	if err != nil {
+		tx.Rollback()
+		http.Error(w, "Inventory update failed", 500)
+		return
+	}
+
+	_, err = tx.Exec("UPDATE users SET balance=balance-$1 WHERE user_id=$2", price, userID)
+	if err != nil {
+		tx.Rollback()
+		http.Error(w, "Payment failed", 500)
+		return
+	}
+
+	_, err = tx.Exec("INSERT INTO orders (user_id, product_id, status) VALUES ($1,$2,'CONFIRMED')", userID, productID)
+	if err != nil {
+		tx.Rollback()
+		http.Error(w, "Order insert failed", 500)
+		return
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		http.Error(w, "Commit failed", 500)
+		return
+	}
+
+	w.Write([]byte("Order placed successfully"))
 }
